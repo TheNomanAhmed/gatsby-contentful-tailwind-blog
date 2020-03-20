5d9009db5027ff97fc81e7ab345dd9651b75ed2c
TheNomanAhmed <nomanahmed478@gmail.com>
2020-03-20 21:29:51 +0200

    initial setup v1.0.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,12 @@ import React from "react"
 const Hero = () => (
   <div className="container py-12 lg:pb-16">
     <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
-      Hello, I'm John{" "}
+      Hello, I'm Noman{" "}
       <span role="img" aria-label="waving hand">
         👋
       </span>
       <br />
-      <span className="text-blue-600">
-        Welcome to my photography portfolio.
-      </span>
+      <span className="text-blue-600">Welcome to my portfolio website.</span>
     </h2>
   </div>
 )
